45b8fd84bce9b1fe935206016d5c91937efbd2ff
hdr_inventory_20230609 <helmutmartinez128@gmail.com>
2023-12-12 16:44:03 -0600

    Segundas pantallas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
+import { ChatComponent } from './screen/chat/chat.component';
 
 const routes: Routes = [
   { path: '', component: HeaderComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'chat', component: ChatComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './screen/chat/chat.component';
+import { SearchComponent } from './screen/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    ChatComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
